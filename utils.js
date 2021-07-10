820126f720c67c395d64ed2bc5889142060442f9
DIG33-Indigo <indigo21team@gmail.com>
2021-07-10 19:19:58 +1000

    added detail to authentication errors to help front end trouble shoot

--- utils.js
+++ utils.js
@@ -28,14 +28,14 @@ class Utils {
         const token = authHeader && authHeader.split(' ')[1]
         if(token == null){
             return res.status(401).json({
-                message: "Unauthorised"
+                message: "Unauthorised: No Token"
             })
         } 
         
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
             if(err) {
                 return res.status(401).json({
-                    message: "Unauthorised"
+                    message: "Unauthorised: Token not valid"
                 })
             }
             req.user = user
