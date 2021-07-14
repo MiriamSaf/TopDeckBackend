c88ab93051a45c51bf794cae4fa5e17880d66d29
DIG33-Indigo <indigo21team@gmail.com>
2021-07-14 18:27:12 +1000

    added longDescription to Package model

--- models/Package.js
+++ models/Package.js
@@ -13,6 +13,10 @@ const packageSchema = new mongoose.Schema({
     type: String,
     required: true    
   },
+  longDescription: {
+    type: String,
+    required: true    
+  },
   date: {
     type: Date,
     required: true
